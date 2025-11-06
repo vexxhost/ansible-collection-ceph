23bd7294cc1834a7dcab215583b6ec5266809e54
Tadas Sutkaitis <tadas.sutkaitis@vexxhost.com>
2025-11-06 16:01:19 +0200

    feat: speedup cephadm commands (#71)
    Signed-off-by: Tadas Sutkaitis <tadas.sutkaitis@vexxhost.com>

--- plugins/module_utils/ca_common.py
+++ plugins/module_utils/ca_common.py
@@ -1,103 +1,128 @@
-import datetime
 import os
+import datetime
+from typing import List
+from ansible.module_utils.basic import AnsibleModule
 
 
-def generate_ceph_cmd(
-    sub_cmd,
-    args,
-    user_key=None,
-    cluster="ceph",
-    user="client.admin",
-    container_image=None,
-    interactive=False,
-):
-    """
+def generate_ceph_cmd(cmd='ceph',
+                 sub_cmd=None,
+                 args=None,
+                 user_key=None,
+                 cluster='ceph',
+                 user='client.admin',
+                 container_image=None,
+                 interactive=False):
+    '''
     Generate 'ceph' command line to execute
-    """
+    '''
 
-    if not user_key:
-        user_key = "/etc/ceph/{}.{}.keyring".format(cluster, user)
+    if user_key is None:
+        user_key = '/etc/ceph/{}.{}.keyring'.format(cluster, user)
 
-    cmd = pre_generate_ceph_cmd(
-        container_image=container_image, interactive=interactive
-    )
+    cmd = pre_generate_ceph_cmd(cmd, container_image=container_image, interactive=interactive)  # noqa: E501
 
-    base_cmd = ["-n", user, "-k", user_key, "--cluster", cluster]
-    base_cmd.extend(sub_cmd)
-    cmd.extend(base_cmd + args)
+    base_cmd = [
+        '-n',
+        user,
+        '-k',
+        user_key,
+        '--cluster',
+        cluster
+    ]
+
+    if sub_cmd is not None:
+        base_cmd.extend(sub_cmd)
+
+    cmd.extend(base_cmd) if args is None else cmd.extend(base_cmd + args)
 
     return cmd
 
 
 def container_exec(binary, container_image, interactive=False):
-    """
+    '''
     Build the docker CLI to run a command inside a container
-    """
+    '''
 
-    container_binary = os.getenv("CEPH_CONTAINER_BINARY")
-    command_exec = [container_binary, "run"]
+    container_binary = os.getenv('CEPH_CONTAINER_BINARY')
+    command_exec = [container_binary, 'run']
 
     if interactive:
-        command_exec.extend(["--interactive"])
+        command_exec.extend(['--interactive'])
 
-    command_exec.extend(
-        [
-            "--rm",
-            "--net=host",
-            "-v",
-            "/etc/ceph:/etc/ceph:z",
-            "-v",
-            "/var/lib/ceph/:/var/lib/ceph/:z",
-            "-v",
-            "/var/log/ceph/:/var/log/ceph/:z",
-            "--entrypoint=" + binary,
-            container_image,
-        ]
-    )
+    command_exec.extend(['--rm',
+                         '--net=host',
+                         '-v', '/etc/ceph:/etc/ceph:z',
+                         '-v', '/var/lib/ceph/:/var/lib/ceph/:z',
+                         '-v', '/var/log/ceph/:/var/log/ceph/:z',
+                         '--entrypoint=' + binary, container_image])
     return command_exec
 
 
 def is_containerized():
-    """
+    '''
     Check if we are running on a containerized cluster
-    """
+    '''
 
-    if "CEPH_CONTAINER_IMAGE" in os.environ:
-        container_image = os.getenv("CEPH_CONTAINER_IMAGE")
+    if 'CEPH_CONTAINER_IMAGE' in os.environ:
+        container_image = os.getenv('CEPH_CONTAINER_IMAGE')
     else:
         container_image = None
 
     return container_image
 
 
-def pre_generate_ceph_cmd(container_image=None, interactive=False):
-    """
-    Generate ceph prefix comaand
-    """
+def pre_generate_ceph_cmd(cmd, container_image=None, interactive=False):
+    '''
+    Generate ceph prefix command
+    '''
     if container_image:
-        cmd = container_exec("ceph", container_image, interactive=interactive)
+        cmd = container_exec(cmd, container_image, interactive=interactive)
     else:
-        cmd = ["ceph"]
+        cmd = [cmd]
 
     return cmd
 
 
-def exec_command(module, cmd, stdin=None):
-    """
+def exec_command(module, cmd, stdin=None, check_rc=False):
+    '''
     Execute command(s)
-    """
+    '''
 
     binary_data = False
     if stdin:
         binary_data = True
-    rc, out, err = module.run_command(cmd, data=stdin, binary_data=binary_data)
+    rc, out, err = module.run_command(cmd, data=stdin, binary_data=binary_data, check_rc=check_rc)  # noqa: E501
 
     return rc, cmd, out, err
 
 
-def exit_module(
-    module, out, rc, cmd, err, startd, changed=False, diff=dict(before="", after="")
-):
+def build_base_cmd(module: "AnsibleModule") -> List[str]:
+    cmd = ['cephadm']
+    docker = module.params.get('docker')
+    image = module.params.get('image')
+    fsid = module.params.get('fsid')
+
+    if docker:
+        cmd.append('--docker')
+    if image:
+        cmd.extend(['--image', image])
+
+    cmd.append('shell')
+
+    if fsid:
+        cmd.extend(['--fsid', fsid])
+
+    return cmd
+
+
+def build_base_cmd_orch(module: "AnsibleModule") -> List[str]:
+    cmd = build_base_cmd(module)
+    cmd.extend(['ceph', 'orch'])
+
+    return cmd
+
+
+def exit_module(module, out, rc, cmd, err, startd, changed=False, diff=dict(before="", after="")):  # noqa: E501
     endd = datetime.datetime.now()
     delta = endd - startd
 
@@ -110,17 +135,17 @@ def exit_module(
         stdout=out.rstrip("\r\n"),
         stderr=err.rstrip("\r\n"),
         changed=changed,
-        diff=diff,
+        diff=diff
     )
     module.exit_json(**result)
 
 
 def fatal(message, module):
-    """
+    '''
     Report a fatal error and exit
-    """
+    '''
 
     if module:
         module.fail_json(msg=message, rc=1)
     else:
-        raise (Exception(message))
+        raise Exception(message)
